6beade9c13ecd264c773a9499e2cda94f958cda3
Krutarth Vanesa <vanesa.k@northeastern.edu>
2020-07-23 17:44:38 -0400

    function updated

--- my_handler.py
+++ my_handler.py
@@ -1,6 +1,9 @@
+import boto3
 def my_handler(event, context):
-    message = 'Hello World,{} {}!'.format(event['first_name'], 
-                                    event['last_name'])  
+    dynamodb = boto3.client('dynamodb')
+    table = client.Table("csye6225")
+    print(table.table_status)
+    message = 'Hello World,{}!'.format(event['Records'][0]['Sns']['Message'])  
     return { 
         'message' : message
     }  
